c27241ad0eaccd395559a0c3ee9527a42372841a
kelvinkuria <kkuria383@yahoo.com>
2025-03-30 23:12:29 +0400

    sunday

--- chapter1/src/App.jsx
+++ chapter1/src/App.jsx
@@ -1,37 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { Fragment } from "react";
+ import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
-function Header(){
-  return(
-<header className="header">
-        <img src="./src/assets/react.svg" alt="React logo" className="logo" />
-        <nav>
-          <ul className="nav-list">
-            <li className="nav-list-item">Pricing</li>
-            <li className="nav-list-item" > About</li>
-            <li className="nav-list-item" >Contact</li>
-          </ul>
-        </nav>
-      </header>
-  )
-}
-function MainContent(){
-return(
-  <main className="main-content">
-        <h1 className="title">Reasons to learn React</h1>
-        <ol className="reasons-list">
-          <li className="reason-item">It's highly in demand</li>
-          <li className="reason-item">Component-based architecture</li>
-          <li className="reason-item">Strong community support</li>
-        </ol>
-      </main>
-)
-}
-function Footer(){
-  return(
-<footer className="footer"> <small>©{new Date().getFullYear()}All rights reserved</small></footer>
-  )
-}
+
 
 
 
---
+++ chapter1/src/components/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer(){
+    return(
+  <footer className="footer"> <small>©{new Date().getFullYear()}All rights reserved</small></footer>
+    )
+  }
