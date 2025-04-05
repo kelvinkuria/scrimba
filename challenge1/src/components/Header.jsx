e0290fbfd44968bc64676e80ce8550bec40b8eab
kelvinkuria <kkuria383@yahoo.com>
2025-04-05 18:56:40 +0400

    done

---
+++ challenge1/src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+function Header() {
+  return (
+    <header className="header">
+        <nav>
+            <img src="./src/assets/react.svg" alt="logo" />
+            <span className='header-title'>reactFacts</span> 
+        </nav>
+    </header>
+  )
+}
+
+export default Header
